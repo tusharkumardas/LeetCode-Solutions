6132ca6b8c62a3869c866a984e44bc5960f5de51
tusharkumardas <tusharkumardas1125@gmail.com>
2026-01-03 01:04:05 +0530

    Added two sum leetcode solutions

---
+++ two_sum.py
@@ -0,0 +1,13 @@
+"""
+LeetCode Problem: Two Sum
+Difficulty: Easy
+"""
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in hashmap:
+                return [hashmap[comp], i]
+            hashmap[nums[i]] = i
